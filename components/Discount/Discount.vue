f426601932506ce8a1be0d355e282c8e8c10bf2d
Jakub Kontra <me@jakubkontra.com>
2020-11-01 09:42:00 +0100

    display expired discounts

--- components/Discount/Discount.vue
+++ components/Discount/Discount.vue
@@ -1,10 +1,11 @@
 <template>
-  <div class="discount">
+  <div :class="computedClass">
     <span class="discount-businessName">{{ discount.businessName }}</span>
-    <h3>{{ discount.discountName }}</h3>
+    <h3><span v-if="!isDiscountValid()">[expired]</span> {{ discount.discountName }}</h3>
     <strong>Category: {{ discount.businessCategory }}</strong> <br>
-    <p><strong>Valid till:</strong> {{ formatDate(discount.validTill).getDate() }}.{{ formatDate(discount.validTill).getMonth() }}. {{ formatDate(discount.validTill).getFullYear() }}</p>
+    <p><strong v-if="isDiscountValid()">Valid till:</strong> {{ formatDate(discount.validTill).getDate() }}.{{ formatDate(discount.validTill).getMonth() }}. {{ formatDate(discount.validTill).getFullYear() }}</p>
     <p><strong>Price:</strong> {{ discount.price }},- CZK</p>
+    
   </div>
 </template>
 
@@ -18,8 +19,26 @@ export default {
       default: () => {}
     }
   },
+  computed: {
+    computedClass() {
+      const d1 = new Date()
+      const d2 = new Date(this.discount.validTill)
+      const isDued = d1.getTime() > d2.getTime()
+
+      return {
+        discount: true,
+        'after-due-date': isDued
+      }
+    }
+  },
   created() {},
   methods: {
+    isDiscountValid() {
+      const d1 = new Date()
+      const d2 = new Date(this.discount.validTill)
+      const isValid = d1.getTime() < d2.getTime()
+      return isValid
+    },
     formatDate(date) {
       const formattedDate = new Date(date)
       return formattedDate
@@ -40,6 +59,10 @@ export default {
     font-weight: bold;
     margin-bottom: 5px;
   }
+}
+
+.after-due-date {
+  background: #deddff;
 }
 
 .discount-businessName {
